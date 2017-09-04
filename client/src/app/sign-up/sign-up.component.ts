3f84db61d9e487628a26ec4cee6369e309974850
IntelliFinder <snirhordan@gmail.com>
2017-09-04 12:06:26 +0300

    Added form fields & functionality

--- client/src/app/sign-up/sign-up.component.ts
+++ client/src/app/sign-up/sign-up.component.ts
@@ -13,14 +13,18 @@ export class SignUpComponent implements OnInit {
       let userRandom = new User('Snirhordan94', 'Senir', 'Hordan', '3498798' );
       console.log("Heres my user" + JSON.stringify(userRandom));
   }
-
+  diagnostic = this.getTrace();
   education = ["Bachelor's Degree", "Master's Degree", "PhD"];
-  dataType = ['Retail', 'Business', 'Educational']
+  dataType = ['Retail', 'Business', 'Educational'];
   model = new User('Anaconda', 'Senir', 'Hordan', '1873298d', this.education[0],
                       this.dataType[0]);
   submit = false;
   onSubmit(){
     this.submit = true;
+  }
+  resetForm(){
+    this.model = new User('', '', '', '', this.education[0],
+                        this.dataType[0]);
   }
   getTrace(){
     return JSON.stringify(this.model);
